ac5d3bea5ac782101b4bcdb3e4e2ade2d8ab80b6
Esteban Beltran <estebanenriquebeltran@gmail.com>
2021-04-03 00:42:34 -0300

    fix cors 2

--- src/main.ts
+++ src/main.ts
@@ -15,10 +15,10 @@ async function bootstrap() {
   console.log(' quye saaleee: ', process.env.NODE_ENV, process.env.PORT);
   app.enableCors({
     credentials: true,
-    origin:
-      process.env.NODE_ENV === 'production'
-        ? 'https://martket-ts-nestjs.vercel.app'
-        : 'http://localhost:3001',
+    origin: 'https://martket-ts-nestjs.vercel.app',
+    // process.env.NODE_ENV === 'production'
+    //   ? 'https://martket-ts-nestjs.vercel.app'
+    //   : 'http://localhost:3001',
   });
 
   await app.listen(process.env.PORT || 3000);
